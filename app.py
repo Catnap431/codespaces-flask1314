5a9737123115857b1603199c21754138e440eaf9
Catnap431 <160397001+Catnap431@users.noreply.github.com>
2024-05-12 12:25:12 +0000

    1 этап проекта

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(name)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/guides')
+def guides():
+    return render_template('guides.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/add_guide', methods=['GET', 'POST'])
+def add_guide():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save('path/to/save/file/' + file.filename)
+        return redirect(url_for('home'))
+    return render_template('add_guide.html')
+
+if name == 'main':
+    app.run(debug=True)
