6e5c11dcf2597b3c3025aab021cd93934742d4f8
Catnap431 <160397001+Catnap431@users.noreply.github.com>
2024-05-19 13:23:16 +0000

    3131

--- app.py
+++ app.py
@@ -48,12 +48,23 @@ def login():
 def add_guide():
     if request.method == 'POST':
         guide_title = request.form.get('guide_title')
-        guide_content = request.form.get('guide_content')
-        guide_file_name = f"{guide_title}.txt"
-        file_path = os.path.join('Save_guides', guide_file_name)
-        with open(file_path, 'w') as file:
-            file.write(guide_content)
-        return redirect(url_for('home'))
+        game_name = request.form.get('game_name')
+        author_name = request.form.get('author_name')
+        description = request.form.get('description')
+        
+        if guide_title and game_name and author_name and description:
+            guide_file_name = f"{guide_title}.txt"
+            file_path = os.path.join('Save_guides', guide_file_name)
+            
+            with open(file_path, 'w') as file:
+                file.write(f"Game Name: {game_name}\n")
+                file.write(f"Author Name: {author_name}\n")
+                file.write(f"Description: {description}\n")
+                file.write("Guide Content:\n")
+                file.write(guide_content)
+            
+            return redirect(url_for('home'))
+    
     return render_template('add_guide.html')
 
 if (__name__) == '__main__':
